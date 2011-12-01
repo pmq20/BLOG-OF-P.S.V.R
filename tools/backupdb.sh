1b2f832dc42248b9b16d88cfa0a110e47c9e63c3
P.S.V.R <pmq2001@gmail.com>
2011-12-01 15:42:29 +0800

    Quick Change

--- tools/backupdb.sh
+++ tools/backupdb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source ../git_sensitive.sh
-mysqldump -u root -p$GIT_SENSITIVE_MYSQL_PASSWORD ofpsvr > ../ofpsvr.backup.sql
+source ./git_sensitive.sh
+mysqldump -u root -p$GIT_SENSITIVE_MYSQL_PASSWORD ofpsvr > ./ofpsvr.backup.sql
